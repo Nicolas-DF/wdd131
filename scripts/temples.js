1e8c3ad54660d98f556314022d4dde7f447064f5
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-07-09 10:38:19 -0300

    Added toggle to the button menu

---
+++ scripts/temples.js
@@ -0,0 +1,8 @@
+const menuButton = document.getElementById('menu-button');
+const navUl = document.querySelector('nav ul');
+
+menuButton.addEventListener('click', () => {
+    navUl.classList.toggle('open');
+})
+
+
