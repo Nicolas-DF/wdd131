67fd7b4e6bf1b99555de92cb8a21ea2795422248
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-07-09 10:39:51 -0300

    Added 'last modified' functionality

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,8 @@ menuButton.addEventListener('click', () => {
 })
 
 
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
