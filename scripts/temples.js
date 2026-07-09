1a6ea0a2e31162a9334951ceac0283dfbf74518b
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-07-09 11:01:17 -0300

    Added an X in case the button is opened

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,15 @@
 const menuButton = document.getElementById('menu-button');
 const navUl = document.querySelector('nav ul');
 
-menuButton.addEventListener('click', () => {
-    navUl.classList.toggle('open');
-})
+menuButton.addEventListener("click", () => {
+    navUl.classList.toggle("open");
+
+    if (navUl.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
 
 
 const currentYear = new Date().getFullYear();
