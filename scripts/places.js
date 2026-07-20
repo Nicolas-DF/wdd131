7eec92a2b9622c53c1e64b850ba83c5412d7d749
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-07-20 09:26:53 -0300

    Created the windChill function and the if statement according to the wind speed and temperature specifications

--- scripts/places.js
+++ scripts/places.js
@@ -5,3 +5,16 @@ const lastModified = document.lastModified;
 document.getElementById("lastmodified").textContent = lastModified;
 
 
+const temperature = 25;
+const windSpeed = 10;
+
+function calculateWindChill(temp, wind) {
+    return 13.12 + (0.6215 * temp) - (11.37 * Math.pow(wind, 0.16)) + (0.3965 * temp * Math.pow(wind, 0.16))
+};
+
+if (temperature <= 10 && windSpeed > 4.8){
+    const windChill = calculateWindChill(temperature, windSpeed)
+    document.getElementById("windChill").textContent = '${windChill.toFixed(1)}°C';
+} else {
+    document.getElementById("windChill").textContent = "N/A";
+}
