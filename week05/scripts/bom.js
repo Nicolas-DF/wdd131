76da0680f28bddcfd175d4a32f9fc053e4437d2c
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-07-29 09:30:54 -0300

    File copied from previous activities

---
+++ week05/scripts/bom.js
@@ -0,0 +1,25 @@
+
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent= input.value;
+        deleteButton.textContent = '❌';
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+    }
+});
