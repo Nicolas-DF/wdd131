baf6a685fd23fda6ac169b6194f5a9c8967fa8dc
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-08-02 17:45:55 -0300

    Got last modified

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,5 @@
 const Year = new Date().getFullYear();
 document.getElementById("currentYear").textContent = Year;
 
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
