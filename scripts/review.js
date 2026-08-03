e475375e49d9a943832a7963f76fdb5b23a87b44
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-08-03 10:01:30 -0300

    Added footer data

--- scripts/review.js
+++ scripts/review.js
@@ -1,3 +1,10 @@
+const Year = new Date().getFullYear();
+document.getElementById("currentYear").textContent = Year;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
+
+
 let counter = Number(localStorage.getItem("numReviews")) || 0;
 counter++;
 localStorage.setItem("numReviews", counter);
