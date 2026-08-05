3618e716285c0a68acca95f8bac60f0ebfeff9dd
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-08-05 09:53:54 -0300

    Form submission handling

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,2 +1,18 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
+
+
+// Form Handling
+document.getElementById("subscriptionForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    if (name && email) {
+        document.getElementById("formMessage").textContent = "Subscription successful! Thank you for subscribing.";
+        this.reset();
+    } else {
+        document.getElementById("formMessage").textContent = "Please, enter at least your full name and email address.";
+    }
+});
