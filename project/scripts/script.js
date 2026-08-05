0e796a63613154c33f1080f6441699484ed53297
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-08-05 10:04:31 -0300

    Check if the email is already registered

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -3,6 +3,8 @@ document.getElementById("currentYear").textContent = currentYear;
 
 
 // Form Handling
+let subscribed = [];
+
 document.getElementById("subscriptionForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
@@ -10,8 +12,14 @@ document.getElementById("subscriptionForm").addEventListener("submit", function(
     const email = document.getElementById("email").value;
 
     if (name && email) {
-        document.getElementById("formMessage").textContent = "Subscription successful! Thank you for subscribing.";
-        this.reset();
+        //Check if the email is already subscribed
+        if (subscribed.includes(email)) {
+            document.getElementById("formMessage").textContent = "You are already subscribed! You will receive our updates soon.";
+        } else {
+            subscribed.push(email);
+            document.getElementById("formMessage").textContent = "Subscription successful! Thank you for subscribing.";
+            this.reset();
+        };
     } else {
         document.getElementById("formMessage").textContent = "Please, enter at least your full name and email address.";
     }
