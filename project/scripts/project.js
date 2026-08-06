25cda47938baddb932d5fe35fb1de877de88d851
Nicolas-DF <nicolas.dfalco@gmail.com>
2026-08-06 09:40:35 -0300

    Prevented error in other pages

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -3,27 +3,35 @@ document.getElementById("currentYear").textContent = currentYear;
 
 
 // Form Handling
-let subscribed = [];
+const form = document.getElementById("subscriptionForm");
 
-document.getElementById("subscriptionForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+if (form) {
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
+        let subscribed = [];
 
-    if (name && email) {
-        //Check if the email is already subscribed
-        if (subscribed.includes(email)) {
-            document.getElementById("formMessage").textContent = "You are already subscribed! You will receive our updates soon.";
-        } else {
-            subscribed.push(email);
-            document.getElementById("formMessage").textContent = "Subscription successful! Thank you for subscribing.";
-            this.reset();
-        };
-    } else {
-        document.getElementById("formMessage").textContent = "Please, enter at least your full name and email address.";
-    }
-});
+        document.getElementById("subscriptionForm").addEventListener("submit", function(e) {
+            e.preventDefault();
+
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+
+            if (name && email) {
+                //Check if the email is already subscribed
+                if (subscribed.includes(email)) {
+                    document.getElementById("formMessage").textContent = "You are already subscribed! You will receive our updates soon.";
+                } else {
+                    subscribed.push(email);
+                    document.getElementById("formMessage").textContent = "Subscription successful! Thank you for subscribing.";
+                    this.reset();
+                };
+            } else {
+                document.getElementById("formMessage").textContent = "Please, enter at least your full name and email address.";
+            }
+        });
+    });
+}
 
 // Menu Toggle
 const toggleButton = document.querySelector(".menu-toggle");
